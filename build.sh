3cedaa05a9b6e4ca8019a4615ad783b5b3f8c1c7
yanosz <yanosz@gmx.net>
2016-07-30 23:59:19 +0200

    Fix: Download from SDK-URL

--- build.sh
+++ build.sh
@@ -21,7 +21,7 @@ IFS=' '
 ## Setting up the build environment
 if ! [ -f "$SDK.tar.bz2" ]
 then 
-  wget  "http://ftp.stw-bonn.de/pub/openwrt/chaos_calmer/15.05.1/ar71xx/generic/OpenWrt-SDK-15.05.1-ar71xx-generic_gcc-4.8-linaro_uClibc-0.9.33.2.Linux-x86_64.tar.bz2" 
+  wget  "$SDK_URL" 
 fi
 
 if ! [ -d "$SDK" ]
